c3900bd57d2169ed1e799ce2ecdca4bc95f830db
zjhlogo <zjhlogo@gmail.com>
2015-02-18 10:03:42 +0800

    add command line parser to project

--- tools/ModelDump/source/main.cpp
+++ tools/ModelDump/source/main.cpp
@@ -8,19 +8,34 @@
 #include "main.h"
 #include "Convertor.h"
 #include <iostream>
+#include <tclap/CmdLine.h>
 
 int main(int argc, char** argv)
 {
-	if (argc < 3)
-	{
-		std::cout << "usage: ModelDump input_file output_file" << std::endl;
+	TCLAP::CmdLine cmd("A tools to dump 3d model files to mesh format", ' ', "1.5.5");
 
+	// add input file argument
+	TCLAP::ValueArg<std::string> inputFileArg("i", "input", "input file", true, "", "path");
+	cmd.add(inputFileArg);
+
+	// add output file argument
+	TCLAP::ValueArg<std::string> outputFileArg("o", "output", "output file", true, "", "path");
+	cmd.add(outputFileArg);
+
+	cmd.parse(argc, argv);
+
+	spank::Convertor conv;
+	if (!conv.openModelFile(inputFileArg.getValue().c_str()))
+	{
+		std::cout << "Error, open file failed, " << inputFileArg.getValue() << std::endl;
 		return 1;
 	}
 
-	spank::Convertor conv;
-	conv.openModelFile(argv[1]);
-	conv.saveFile(argv[2]);
+	if (!conv.saveFile(outputFileArg.getValue().c_str()))
+	{
+		std::cout << "Error, save file failed, " << outputFileArg.getValue() << std::endl;
+		return 1;
+	}
 
 	return 0;
 }
